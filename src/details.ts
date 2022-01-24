86188c086681fda329695f688cc723b2f402c1e8
Sahil Patel <sahilshahpatel@gmail.com>
2022-01-24 20:13:08 +0100

    Basic details filler

--- src/details.ts
+++ src/details.ts
@@ -1,4 +1,5 @@
-import { AppState } from "./app-state";
+import * as d3 from 'd3';
+import { AppState, Student } from "./app-state";
 
 /** Logic for the details panel showing data about a single selected student. */
 export class DetailsController {
@@ -8,6 +9,42 @@ export class DetailsController {
     private readonly appState: AppState,
   ) {
     console.log('DetailsController initialized with:', element);
+
+    this.appState.observeHovered(st => this.fillDataParagraph(st));
   }
 
+  // Old version
+  private fillDataTable(student: Student) {
+    if (student === null) return;
+
+    const data = Object.entries(student);
+
+    const table = d3.select("#details").select("table").select("tbody");
+
+    table.selectAll("*").remove();
+
+    const rows = table.selectAll("tr")
+      .data(data)
+      .enter().append("tr")
+    
+    rows.append("th").text(d => d[0])
+    rows.insert("td").text(d => d[1])
+  }
+
+  private fillDataParagraph(student: Student) {
+    if (student === null) return;
+
+    const data = Object.entries(student);
+    const container = d3.select("#details").select("div.details");
+    container.selectAll("*").remove();
+
+    const rows = container.selectAll("div")
+      .data(data)
+      .enter().append("div")
+
+    rows.append("p").classed("fs-6 fw-bold mb-0", true).text(d => d[0]);
+    rows.insert("p").classed("fs-6", true).text(d => {
+      return d[1] !== null ? d[1] : "-";
+    })
+  }
 }
