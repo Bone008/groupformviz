1840096b45894d58e9a9fdd869960776a1748bbe
Lukas <Bone008@users.noreply.github.com>
2022-01-26 00:13:51 +0100

    Colorize histogram based on group selection + add mean and median.

--- src/visualization.ts
+++ src/visualization.ts
@@ -7,6 +7,15 @@ import { populateSkillSelector, Student } from './util';
 //@ts-ignore
 import * as rawInterests from './interests.csv'
 
+const HIST_WIDTH = 360;
+const HIST_HEIGHT = 200;
+
+interface HistogramResult {
+  data: number[];
+  median: number;
+  mean: number;
+}
+
 /** Logic for the main visualization view */
 export class VisualizationController {
   private readonly radarController: Radar;
@@ -30,6 +39,9 @@ export class VisualizationController {
     this.setSelectedSkill(skillSelect.value);
     this.renderHistogram();
 
+    this.appState.observeSelected(() => this.renderHistogram());
+    this.appState.observeActive(() => this.renderHistogram());
+
     this.renderWordCloud();
   }
 
@@ -47,70 +59,139 @@ export class VisualizationController {
     // Clear before rerendering, should be done more intelligently in the future.
     svg.selectAll('*').remove();
 
-    const width = 360;
-    const height = 200;
-    const histogram = this.buildSkillHistogram(this.selectedSkill);
+    const histogram = this.buildSkillHistogram(this.selectedSkill, this.appState.students);
+    const groupHistogram = this.buildSkillHistogram(this.selectedSkill, this.appState.selected);
+
+    const activeIsInGroup = this.appState.selected.includes(this.appState.active);
+    let activeHistogram: HistogramResult;
+    if (activeIsInGroup) {
+      // If in group --> render only active in front
+      activeHistogram = this.buildSkillHistogram(this.selectedSkill,
+        [this.appState.active].filter(x => !!x));
+    } else {
+      // If not in group --> render active cumulative with group behind
+      activeHistogram = this.buildSkillHistogram(this.selectedSkill,
+        [...this.appState.selected, this.appState.active].filter(x => !!x));
+    }
 
     // X axis
     const xScale = d3.scaleBand<number>()
-      .range([0, width])
-      .domain(d3.range(histogram.length))
+      .range([0, HIST_WIDTH])
+      .domain(d3.range(histogram.data.length))
       .padding(0.2);
     svg.append('g')
-      .attr('transform', 'translate(0,' + height + ')')
+      .attr('transform', 'translate(0,' + HIST_HEIGHT + ')')
       .call(d3.axisBottom(xScale))
       .selectAll('text')
       .style('text-anchor', 'middle');
 
     svg.append('text')
       .attr("text-anchor", "middle")
-      .attr("x", width / 2)
-      .attr("y", height + 30)
+      .attr("x", HIST_WIDTH / 2)
+      .attr("y", HIST_HEIGHT + 30)
       .attr('font-size', '11')
       .text('Skill proficiency');
 
     // Add Y axis
     const yScale = d3.scaleLinear()
-      .domain([0, Math.max(10, d3.max(histogram))])
-      .range([height, 0]);
+      .domain([0, Math.max(10, d3.max(histogram.data))])
+      .range([HIST_HEIGHT, 0]);
     svg.append('g')
       .call(d3.axisLeft(yScale));
 
     svg.append('text')
       .attr("text-anchor", "middle")
-      .attr('x', -height / 2)
+      .attr('x', -HIST_HEIGHT / 2)
       .attr("y", -25)
       .attr('font-size', '12')
       .attr("transform", "rotate(-90)")
       .text('Number of students');
 
     // Bars
-    svg.selectAll('mybar')
-      .data(histogram)
-      .enter()
-      .append('g')
-      .call(g => g.append('rect')
-        .attr('x', (d, i) => xScale(i))
-        .attr('y', (d, i) => yScale(d))
-        .attr('width', xScale.bandwidth())
-        .attr('height', d => height - yScale(d))
-        .attr('fill', '#69b3a2')
-      )
-      .call(g => g.append('title')
-        .text((d, i) => this.appState.students
-          .filter(student => student[this.selectedSkill] === i)
-          .map(student => student.Alias)
-          .join(', '))
-      );
+    const barPlots = [
+      { histogram: histogram, color: '#446' },
+      { histogram: activeHistogram, color: 'rgba(255, 153, 153, 0.5)' },
+      { histogram: groupHistogram, color: '#69b3a2' },
+    ];
+    if (activeIsInGroup) {
+      // Swap draw order so active is drawn AFTER group.
+      const tmp = barPlots[1];
+      barPlots[1] = barPlots[2];
+      barPlots[2] = tmp;
+    }
+    for (const { histogram, color } of barPlots) {
+      svg.selectAll('mybar')
+        .data(histogram.data)
+        .enter()
+        .append('g')
+        .call(g => g.append('rect')
+          .attr('x', (d, i) => xScale(i))
+          .attr('y', (d, i) => yScale(d))
+          .attr('width', xScale.bandwidth())
+          .attr('height', d => HIST_HEIGHT - yScale(d))
+          .attr('fill', color)
+        )
+        .call(g => g.append('title')
+          .text((d, i) => this.appState.students
+            .filter(student => student[this.selectedSkill] === i)
+            .map(student => student.Alias)
+            .sort()
+            .join(', '))
+        );
+    }
+
+    // Mean
+    this.drawMean(svg, xScale, histogram.mean, 'Global mean', '#112');
+    // Decided against drawing group mean, too cluttered
+    // if (groupHistogram.mean) {
+    //   this.drawMean(svg, xScale, groupHistogram.mean, '', '#336b51');
+    // }
+
+
+    this.element.querySelector<HTMLElement>('.value-globalMean').textContent = histogram.mean.toFixed(2);
+    this.element.querySelector<HTMLElement>('.value-globalMedian').textContent = histogram.median.toFixed(2);
+    this.element.querySelector<HTMLElement>('.value-groupMean').textContent = groupHistogram.mean?.toFixed(2) || 'N/A';
+    this.element.querySelector<HTMLElement>('.value-groupMedian').textContent = groupHistogram.median?.toFixed(2) || 'N/A';
   }
 
-  private buildSkillHistogram(skill: keyof Student): number[] {
-    const skills: number[] = Array(11).fill(0);
-    for (const student of this.appState.students) {
+  private drawMean(
+    svg: d3.Selection<d3.BaseType, unknown, null, undefined>,
+    xScale: d3.ScaleBand<number>,
+    mean: number,
+    label: string,
+    color: string) //
+  {
+    const meanFloor = Math.floor(mean);
+    const meanX = xScale(meanFloor) + (0.5 + mean - meanFloor) * xScale.bandwidth();
+    svg.append('line')
+      .attr('x1', meanX)
+      .attr('x2', meanX)
+      .attr('y1', -8)
+      .attr('y2', HIST_HEIGHT)
+      .style('stroke', color)
+      .style('stroke-width', 2)
+      .style('stroke-dasharray', '5,3');
+    svg.append('text')
+      .attr("text-anchor", "start")
+      .attr('x', meanX + 2)
+      .attr("y", -1)
+      .attr('font-size', '12')
+      .attr('fill', color)
+      .text(label);
+  }
+
+  private buildSkillHistogram(skill: keyof Student, students: Student[]): HistogramResult {
+    const data: number[] = Array(11).fill(0);
+    for (const student of students) {
       const value = Number(student[skill]);
-      skills[value] = (skills[value] || 0) + 1;
+      data[value] = (data[value] || 0) + 1;
     }
-    return skills;
+    const values = students.map(s => Number(s[skill]));
+    return {
+      data,
+      mean: d3.mean(values),
+      median: d3.median(values),
+    }
   }
 
   private renderWordCloud() {
